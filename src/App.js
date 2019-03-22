f6108b9d2a46dd3d4d2b06263731a01febee9bc4
Andrea Arias <ariasdrea@gmail.com>
2019-03-22 17:19:44 +0100

    modified state, removed unused methods, and modified position of metatron tooltip

--- src/App.js
+++ src/App.js
@@ -9,58 +9,23 @@ class App extends Component {
       videos: [
         {
           id: 0,
-          title: "Metatron",
-          duration: 0,
-          url: "https://www.youtube.com/watch?v=VFfKPZhY16Q",
-          img: "img.png",
-          hotspot: "music starts"
+          url: "https://www.youtube.com/watch?v=VFfKPZhY16Q"
         },
         {
           id: 1,
-          title: "People",
-          duration: 0,
-          url: "https://www.youtube.com/watch?v=ZTGIzeye-xw",
-          img: "img.png"
+          url: "https://www.youtube.com/watch?v=ZTGIzeye-xw"
         },
         {
           id: 2,
-          title: "Nightcall",
-          duration: 0,
           url: "https://www.youtube.com/watch?v=30jrmzzgHLc"
         },
         {
           id: 3,
-          title: "Gold",
-          duration: 0,
           url: "https://www.youtube.com/watch?v=hi4pzKvuEQM"
         }
       ]
     };
-
-    this.onMouseEnter = this.onMouseEnter.bind(this);
-    this.onPlay = this.onPlay.bind(this);
   }
-
-  onMouseEnter(e) {
-    console.log("mouseenter works");
-    console.log("e.currentTarget", e.currentTarget);
-  }
-
-  onPlay(e) {
-    console.log("onPlay");
-    console.log("e on onPlay:", e);
-  }
-
-  onProgress = val => {
-    // console.log("onProgress", val);
-    if (val.playedSeconds > 17 && val.playedSeconds < 18) {
-      console.log("music starts");
-    }
-  };
-
-  onDuration = duration => {
-    console.log("onDuration", duration);
-  };
 
   render() {
     return (
@@ -75,9 +40,6 @@ class App extends Component {
                 controls
                 height="50%"
                 width="50%"
-                onPlay={this.onPlay}
-                onProgress={this.onProgress}
-                onMouseEnter={this.onMouseEnter}
               />
             );
           })}
